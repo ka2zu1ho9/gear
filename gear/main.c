39fbf9a09d4e8865723b07c7a5cbf111a9a42bf1
kenmochi kazuho <kenmochi20030219kazuho@gmail.com>
2021-05-16 13:41:21 +0900

    feat : noを選択するまで、計算ができるようにwhileを追加

--- gear/main.c
+++ gear/main.c
@@ -2,8 +2,23 @@
 #include "gear.h"
 
 int main(void){
+    int is_keeping_while = 1;
+    int cnt = 0;
 
-    calculate_tooth_combination();
+    while(1){
+        if(cnt > 0){
+            printf("Do you want to continue the calculation?\n");
+            printf("yes:0,no:1\n");
+            scanf("%d", &is_keeping_while);   
+            printf("\n");
+
+            if(is_keeping_while == 1){
+                break;
+            }
+        }
+        calculate_tooth_combination();
+        cnt ++;
+    }
 
     return 0;
 }
